bc3a23dc103995164d3137c20de977707581b627
john <john@filimin.com>
2022-08-12 13:16:10 -0500

    send compressed info when rsyncing

--- pullall.sh
+++ pullall.sh
@@ -2,6 +2,6 @@
 for i in {1..12}
 do
     echo "rsyncing 172.16.1.$i"
-    rsync -avh *.sh *.pd *.mp4 user@172.16.1.$i:~/Desktop/tv-jumbotron/.
+    rsync -avhz *.sh *.pd *.mp4 user@172.16.1.$i:~/Desktop/tv-jumbotron/.
 done
 
