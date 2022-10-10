cc2ad0695a2447ecbe1786758153505af8059172
John <john.harrison@alum.mit.edu>
2022-10-10 10:27:19 -0500

    tweak up scripts

--- pullall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for i in {1..12}
-do
-    echo "rsyncing 172.16.1.$i"
-    rsync -avhz *.sh *.pd *.mp4 user@172.16.1.$i:~/Desktop/tv-jumbotron/.
-done
-
---
+++ rsyncall.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+for i in {1..12}
+do
+    echo "rsyncing 172.16.1.$i"
+    rsync -avhz --progress *.sh *.pd *.mp4 user@172.16.1.$i:~/Desktop/tv-jumbotron/.
+done
+
